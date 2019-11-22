fc17febc0a0b1c1063af0e3a83198a2403ba5af4
shubhamdholu-code <shubham.dholu@moonfroglabs.com>
2019-11-22 17:42:48 +0530

    Hell Yeah

--- recipie_react/src/App.js
+++ recipie_react/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
+import Recipie from './Recipie';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = "9cf698bb";
+  const APP_KEY = "0199fcd111f2746f5920e437c788fc51";
+
+  const [recipies, setRecipies] = useState([]);
+  const [search,setSearch] = useState('');
+  const [query, setQuery] =useState('chicken');
+
+
+useEffect(() => {
+  getRecipies();
+  },[query]);
+
+  const getRecipies = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    setRecipies(data.hits);
+    console.log(data.hits);
+  };
+
+const updateSearch = e =>{
+  setSearch(e.target.value);
+  
+};
+
+const getSearch = e =>{
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');  
+}
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={getSearch} className="search-form">
+      <input className ="search-bar" type="text" value={search} onChange={updateSearch} />
+      <button className="search-button" type="submit">Search</button>
+    </form> 
+    <div className="recipes">
+     {recipies.map(recipie =>(
+      <Recipie 
+      key={recipie.recipe.label}
+        title={recipie.recipe.label} 
+        calories={recipie.recipe.calories}
+        image ={recipie.recipe.image}
+        ingredients={recipie.recipe.ingredients}
+      />  
+     ))}
+    </div>
     </div>
   );
-}
+};
 
 export default App;
